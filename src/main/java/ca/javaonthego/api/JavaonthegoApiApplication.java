2f7c1d5a3569cf30ab7e18d50082e60ac00ba434
jfaulk <james.faulk@georgebrown.ca>
2016-01-25 21:34:59 -0500

    Initial commit.

---
+++ src/main/java/ca/javaonthego/api/JavaonthegoApiApplication.java
@@ -0,0 +1,12 @@
+package ca.javaonthego.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaonthegoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaonthegoApiApplication.class, args);
+    }
+}
---
+++ src/test/java/ca/javaonthego/api/JavaonthegoApiApplicationTests.java
@@ -0,0 +1,18 @@
+package ca.javaonthego.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JavaonthegoApiApplication.class)
+@WebAppConfiguration
+public class JavaonthegoApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
