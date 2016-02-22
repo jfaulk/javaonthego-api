1708bc309657c924e536272159947f8a740489ba
jfaulk <james.faulk@georgebrown.ca>
2016-02-22 04:19:08 -0500

    add update and delete methods to user controller

--- src/main/java/ca/javaonthego/api/controller/UserController.java
+++ src/main/java/ca/javaonthego/api/controller/UserController.java
@@ -12,28 +12,50 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
- * Created by james on 2016-02-10.
+ * User Controller exposes a series of RESTful endpoints
  */
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Get user using id. Returns HTTP 404 if user not found
+     *
+     * @param userId
+     * @return retrieved user
+     */
     @RequestMapping(value = "/api/users/{userId}", method = RequestMethod.GET)
     public User getUser(@PathVariable("userId") Long userId) {
         return userRepository.findOne(userId);
     }
 
-    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
-    public List<User> findAllUsers() {
-        return (List<User>) userRepository.findAll();
-    }
-
+    /**
+     * Get user using username. Returns HTTP 404 if user not found
+     *
+     * @param userName
+     * @return retrieved user
+     */
     @RequestMapping(value = "/api/user/{userName}", method = RequestMethod.GET)
     public List<User> getUserByUsername(@PathVariable("userName") String userName) {
         return userRepository.findByUserName(userName);
     }
 
+    /**
+     * Gets all users.
+     *
+     * @return the users
+     */
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public List<User> findAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    /**
+     * Create a new user and return in response with HTTP 201
+     *
+     * @return created customer
+     */
     @RequestMapping(value = "/api/users", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public User createUser(@RequestBody User user, HttpServletResponse httpResponse, WebRequest request) {
@@ -43,5 +65,37 @@ public class UserController {
         httpResponse.setHeader("Location", String.format("%s/api/users/%s", request.getContextPath(), user.getId()));
 
         return createdUser;
+    }
+
+    /**
+     * Update user with given user id.
+     *
+     * @param userId the customer
+     */
+    @RequestMapping(value = {"/api/users/{userId}"}, method = {RequestMethod.PUT})
+    public void updateUser(@RequestBody User user, @PathVariable("userId") Long userId,
+                           HttpServletResponse httpResponse) {
+
+        if (!userRepository.exists(userId)) {
+            httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        } else {
+            userRepository.save(user);
+            httpResponse.setStatus(HttpStatus.NO_CONTENT.value());
+        }
+    }
+
+    /**
+     * Deletes the user with given user id if it exists and returns HTTP 204.
+     *
+     * @param userId the user id
+     */
+    @RequestMapping(value = "/api/users/{userId}", method = RequestMethod.DELETE)
+    public void removeUser(@PathVariable("userId") Long userId, HttpServletResponse httpResponse) {
+
+        if (userRepository.exists(userId)) {
+            userRepository.delete(userId);
+        }
+
+        httpResponse.setStatus(HttpStatus.NO_CONTENT.value());
     }
 }
