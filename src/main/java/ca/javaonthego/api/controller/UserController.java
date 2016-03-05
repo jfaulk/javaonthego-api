bb30c0860cfb49cabdd450ccc580916bc8ab70bc
jfaulk <james.faulk@georgebrown.ca>
2016-03-05 02:07:16 -0500

    allow cross origin. allow api to consume html forms

--- src/main/java/ca/javaonthego/api/controller/UserController.java
+++ src/main/java/ca/javaonthego/api/controller/UserController.java
@@ -46,6 +46,7 @@ public class UserController {
      *
      * @return the users
      */
+    @CrossOrigin
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
     public List<User> findAllUsers() {
         return (List<User>) userRepository.findAll();
@@ -56,7 +57,8 @@ public class UserController {
      *
      * @return created customer
      */
-    @RequestMapping(value = "/api/users", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin
+    @RequestMapping(value = "/api/users", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     @ResponseBody
     public User createUser(@RequestBody User user, HttpServletResponse httpResponse, WebRequest request) {
         User createdUser;
@@ -72,6 +74,7 @@ public class UserController {
      *
      * @param userId the customer
      */
+    @CrossOrigin
     @RequestMapping(value = {"/api/users/{userId}"}, method = {RequestMethod.PUT})
     public void updateUser(@RequestBody User user, @PathVariable("userId") Long userId,
                            HttpServletResponse httpResponse) {
@@ -84,11 +87,19 @@ public class UserController {
         }
     }
 
+    @RequestMapping(value = {"/api/user/{userName}"}, method = {RequestMethod.PUT})
+    public void updateUserByUserName(@RequestBody User user, @PathVariable("userName") String userName,
+                                     HttpServletResponse httpResponse) {
+        userRepository.save(user);
+        httpResponse.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+
     /**
      * Deletes the user with given user id if it exists and returns HTTP 204.
      *
      * @param userId the user id
      */
+    @CrossOrigin
     @RequestMapping(value = "/api/users/{userId}", method = RequestMethod.DELETE)
     public void removeUser(@PathVariable("userId") Long userId, HttpServletResponse httpResponse) {
 
